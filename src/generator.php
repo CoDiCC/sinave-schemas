f693f3bc68836e30344980ebdfb108cc172c1e46
Estevão Soares dos Santos <estevao.santos@gmail.com>
2017-09-29 12:38:41 +0100

    feat(sifilis): add sifilis schema (#1)

--- src/generator.php
+++ src/generator.php
@@ -1,12 +1,25 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: estevao.santos
- * Date: 25/09/2017
- * Time: 16:56
+ * generator.php
+ *
+ * CoDiCC - SINAVE Schemas <https://github.com/CoDiCC/sinave-schemas>
+ * Sinave Schemas for Communicable Disease Case Classifier Library
+ *
+ * ---
+ *
+ * Copyright (C) 2017 Estevão Soares dos Santos - All Rights Reserved
+ *
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license.
+ * You should have received a copy of the MIT license with
+ * this file. If not, please write visit :
+ * https://github.com/CoDiCC/sinave-schemas/blob/master/LICENSE
+ *
  */
 
 require_once(__DIR__ . '/../vendor/autoload.php');
+
+use \Symfony\Component\Yaml\Yaml;
 
 
 function getIncludes()
@@ -17,14 +30,22 @@ function getIncludes()
         return $yml;
     }
 
+    // first get the includes
     $directory = new DirectoryIterator(realpath(__DIR__ . '/includes'));
     foreach ($directory as $f) {
         if ($f->isDot() || $f->isDir()) continue;
         $yml .= "\n" . file_get_contents($f->getRealPath());
     }
+
+    // then get partials
+    $directory = new DirectoryIterator(realpath(__DIR__ . '/partials'));
+    foreach ($directory as $f) {
+        if ($f->isDot() || $f->isDir()) continue;
+        $yml .= "\n" . file_get_contents($f->getRealPath());
+    }
+
     return $yml;
 }
-
 
 function recurseDirectories(string $path)
 {
@@ -36,33 +57,42 @@ function recurseDirectories(string $path)
         if ($f->isDir()) {
             //new directory iterator and recurse
         } else {
-            $filepath = realpath(__DIR__ . '/../dist/');
-            $filename = str_replace('.yml', '.json', $f->getFilename());
+            $filename = str_replace('.yml', '', $f->getFilename());
+            $filepath = realpath(__DIR__ . '/../dist/') . '/' . $filename;
             $schema = readSchema($f->getRealPath());
-            createJson($schema, realpath($filepath . '/' . $filename));
+            createYaml($schema, $filepath);
+            createJson($schema, $filepath);
         }
     }
 }
 
-function readSchema(string $filepath)
+function readSchema(string $filepath, bool $includeBase = true)
 {
     $yml = getIncludes() . file_get_contents($filepath);
-    $schema = \Symfony\Component\Yaml\Yaml::parse($yml);
-    return $schema;
+    $schema = Yaml::parse($yml);
+    $newSchema = [];
+    foreach ($schema as $key => $val) {
+      if (substr($key,0,1) === '@') {
+        continue;
+      }
+      $newSchema[$key] = $val;
+    }
+    return $newSchema;
 }
 
 
 function createJson(array $schema, string $path)
 {
+    $path = $path . '.json';
     $schema['id'] = str_replace('.yml', '.json', $schema['id']);
     return file_put_contents($path, json_encode($schema, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES));
 }
 
+function createYaml(array $schema, string $path)
+{
+    $path = $path . '.yml';
+    return file_put_contents($path, Yaml::dump($schema, 6, 2));
+}
 
 recurseDirectories(realpath(__DIR__ . '/schemas'));
 
-
-
-
-
-//
